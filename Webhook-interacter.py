1bacfc673a08387bba2f7cd1da671c50c7dba27e
websterwh16 <81191122+websterwh16@users.noreply.github.com>
2021-11-09 18:57:12 -0800

    Update Webhook-interacter.py

--- Webhook-interacter.py
+++ Webhook-interacter.py
@@ -5,16 +5,18 @@ from os import system
 title = "Webook Interacter"
 system("title "+title)
 os.system('cls')
-webhook = "PUT YOUR WEBHOOK HERE"
+webhook = "https://canary.discord.com/api/webhooks/907825829889855519/4pyYfPUtlCZK22ROPDahi2fmsGmMSJaEC6w_Jowzq4nNfahvbYIj3fIcUDmYuewnWCc4"
 
+hook = Webhook(webhook)
+os.system("cls")
+print("Enter text to send")
+print("Type exit to leave")
 while True:
-    hook = Webhook(webhook)
-
-    text = input("Enter text to sent: ")
+    text = input("|")
     if text.lower() == "exit":
         break
+    if text == "":
+        text = "᲼"
     hook.send(text)
-    print(f"Sent {text} to webhook " )
-    time.sleep(1)
-    os.system('cls')
+    print(f"> {text}" )
 
